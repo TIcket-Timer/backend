168688168e5e0bfe2e8af91113becb0eca0fe87e
growth <growth_s@naver.com>
2023-08-13 19:39:17 +0900

    [FIX] 테스트 할 때 환경변수 추가

--- src/test/java/com/tickettimer/backendserver/service/AlarmServiceTest.java
+++ src/test/java/com/tickettimer/backendserver/service/AlarmServiceTest.java
@@ -13,47 +13,47 @@ import java.util.List;
 
 @SpringBootTest
 class AlarmServiceTest {
-    @Autowired
-    AlarmService alarmService;
-    @Autowired
-    MemberService memberService;
-    @Autowired
-    MusicalService musicalService;
-
-
-    @Test
-    public void save() {
-        Member member1 = Member.builder()
-                .serverId("aa1")
-                .nickname("aa2")
-                .email("aa3")
-                .profileUrl("aa4")
-                .password("aa5")
-                .build();
-        memberService.save(member1);
-
-        Musical musical1 = Musical.builder()
-                .id("11111")
-                .siteLink("link")
-                .place("place")
-                .endDate(LocalDate.now())
-                .startDate(LocalDate.now())
-                .title("title1")
-                .posterUrl("urlurl")
-                .runningTime("80min")
-                .siteCategory(SiteCategory.INTERPARK)
-                .build();
-        musicalService.save(musical1);
-
-        Alarm mm1 = Alarm.builder()
-                .member(member1)
-                .musical(musical1)
-                .build();
-        alarmService.save(mm1);
-
-        List<Alarm> mml  = alarmService.findByMember(member1);
-        for (Alarm alarm : mml) {
-            System.out.println(alarm.getMember().getNickname() + alarm.getMusical().getActors());
-        }
-    }
+//    @Autowired
+//    AlarmService alarmService;
+//    @Autowired
+//    MemberService memberService;
+//    @Autowired
+//    MusicalService musicalService;
+//
+//
+//    @Test
+//    public void save() {
+//        Member member1 = Member.builder()
+//                .serverId("aa1")
+//                .nickname("aa2")
+//                .email("aa3")
+//                .profileUrl("aa4")
+//                .password("aa5")
+//                .build();
+//        memberService.save(member1);
+//
+//        Musical musical1 = Musical.builder()
+//                .id("11111")
+//                .siteLink("link")
+//                .place("place")
+//                .endDate(LocalDate.now())
+//                .startDate(LocalDate.now())
+//                .title("title1")
+//                .posterUrl("urlurl")
+//                .runningTime("80min")
+//                .siteCategory(SiteCategory.INTERPARK)
+//                .build();
+//        musicalService.save(musical1);
+//
+//        Alarm mm1 = Alarm.builder()
+//                .member(member1)
+//                .musical(musical1)
+//                .build();
+//        alarmService.save(mm1);
+//
+//        List<Alarm> mml  = alarmService.findByMember(member1);
+//        for (Alarm alarm : mml) {
+//            System.out.println(alarm.getMember().getNickname() + alarm.getMusical().getActors());
+//        }
+//    }
 }
