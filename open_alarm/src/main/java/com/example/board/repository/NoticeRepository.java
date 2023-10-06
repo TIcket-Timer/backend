45eac900f0d1fa433c5d4fbc3412b2bc0397bcab
growth <growth_s@naver.com>
2023-10-06 16:26:21 +0900

    [CHORE] 수정

--- open_alarm/src/main/java/com/example/board/repository/NoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.domain.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
